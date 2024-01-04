b1ea11af5df2b741a0f960e785a9edae40592b04
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-04 10:54:10 +0900

    형변환 연산자

--- src/StandardOfJava/chapter03/Ex3_5.java
+++ src/StandardOfJava/chapter03/Ex3_5.java
@@ -1,2 +1,10 @@
-package StandardOfJava.chapter03;public class Ex3_5 {
+package StandardOfJava.chapter03;
+
+public class Ex3_5 {
+    public static void main(String[] args){
+        double d = 85.7;
+        int score = (int)d;
+        System.out.println("score = " + score);
+        System.out.println("d = "  + d);
+    }
 }
