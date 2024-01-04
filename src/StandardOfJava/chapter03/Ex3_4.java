9a6fbfeb0cee8d273347cbc72b3fe16576d94ad2
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-04 10:53:09 +0900

    형변환 연산자

--- src/StandardOfJava/chapter03/Ex3_4.java
+++ src/StandardOfJava/chapter03/Ex3_4.java
@@ -1,2 +1,14 @@
-package StandardOfJava.chapter03;public class Ex3_4 {
+package StandardOfJava.chapter03;
+
+public class Ex3_4 {
+    public static void main(String[] args){
+        int i = -10;
+        i = +i;
+        System.out.println(i);
+
+        i = -10;
+        i = -i;
+        System.out.println(i);
+
+    }
 }
