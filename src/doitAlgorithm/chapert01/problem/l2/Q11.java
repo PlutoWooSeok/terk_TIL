60c5c0e827de86f799c343cc3e7ac2713ca4c338
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-08 13:02:53 +0900

    docs: 사후 판단 반복 연습문제

--- src/doitAlgorithm/chapert01/problem/l2/Q11.java
+++ src/doitAlgorithm/chapert01/problem/l2/Q11.java
@@ -1,2 +1,26 @@
-package doitAlgorithm.chapert01.problem.l2;public class Q11 {
+package doitAlgorithm.chapert01.problem.l2;
+
+import java.util.Scanner;
+
+public class Q11 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("양의 정숫값의 자릿수를 구합니다.");
+
+        int n;
+        do {
+            System.out.print("정숫값 : ");
+            n = stdIn.nextInt();
+        } while (n <= 0);
+
+        int no = 0;         // 자릿수
+        while (n > 0) {
+            n /= 10;         // n을 10으로 나눕니다
+            no++;
+        }
+
+        System.out.println("그 수는 " + no + "자리입니다.");
+    }
 }
+
