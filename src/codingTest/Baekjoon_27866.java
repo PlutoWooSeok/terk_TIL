e47ebf2faf9c6ee7f05371286c89cf544569880d
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-09 16:28:03 +0900

    문자열_문자와 문자열_27866

--- src/codingTest/Baekjoon_27866.java
+++ src/codingTest/Baekjoon_27866.java
@@ -1,2 +1,16 @@
-package codingTest;public class Baekjoon_27866 {
+package codingTest;
+
+import java.util.Scanner;
+
+public class Baekjoon_27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String[] arrStr =  str.split("");
+
+        int N = sc.nextInt();
+
+        System.out.println(arrStr[N-1]);
+
+    }
 }
