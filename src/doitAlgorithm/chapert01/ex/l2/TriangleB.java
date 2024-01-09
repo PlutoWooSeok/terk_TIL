3bec36662df8e7eb47045324037596fab416baad
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-09 10:55:33 +0900

    docs: 다중 루프문을 이용한 이등변 삼각형 출력

--- src/doitAlgorithm/chapert01/ex/l2/TriangleB.java
+++ src/doitAlgorithm/chapert01/ex/l2/TriangleB.java
@@ -1,2 +1,26 @@
-package doitAlgorithm.chapert01.ex.l2;public class TriangleB {
+package doitAlgorithm.chapert01.ex.l2;
+
+import java.util.Scanner;
+
+public class TriangleB {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int n;
+
+        System.out.println("왼쪽 아래가 직각인 이등변 삼각형을 출력합니다.");
+
+        do {
+            System.out.print("몇 단 삼각형입니까? : ");
+            n = stdIn.nextInt();
+        } while (n <= 0);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+    }
 }
