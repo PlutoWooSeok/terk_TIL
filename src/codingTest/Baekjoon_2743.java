2149f5825a616588403688199761e96186d92faa
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-09 16:28:34 +0900

    문자열_단어 길이 재기_2743

--- src/codingTest/Baekjoon_2743.java
+++ src/codingTest/Baekjoon_2743.java
@@ -1,2 +1,13 @@
-package codingTest;public class Baekjoon_2743 {
+package codingTest;
+
+import java.util.Scanner;
+
+public class Baekjoon_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        System.out.println(str.length());
+    }
 }
