8d992ad4562bb390ef956271c7ab9647d7f79e48
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-09 16:29:01 +0900

    문자열_문자열-9086

--- src/codingTest/Baekjoon_9086.java
+++ src/codingTest/Baekjoon_9086.java
@@ -1,2 +1,18 @@
-package codingTest;public class Baekjoon_9086 {
+package codingTest;
+
+import java.util.Scanner;
+
+public class Baekjoon_9086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            String st = sc.next();
+            String[] strArr = st.split("");
+            System.out.println(strArr[0]+strArr[strArr.length-1]);
+        }
+
+
+    }
 }
