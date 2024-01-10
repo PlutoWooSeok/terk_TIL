1c83e47ed7211beba0f38281d6247a2314381176
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-10 12:08:05 +0900

    style: 변동X

--- src/codingTest/Programers_Conversion.java
+++ src/codingTest/Programers_Conversion.java
@@ -3,7 +3,7 @@ package codingTest;
 
 public class Programers_Conversion {
     public  static void main(String[] args){
-        String str = "-1234";
+        String str = "1234";
 
         int N = Integer.parseInt(str);
 
