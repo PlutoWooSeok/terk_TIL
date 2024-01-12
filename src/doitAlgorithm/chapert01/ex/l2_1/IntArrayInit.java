92cce4cee7a5a701ef62c330b3229e4e2d2f540a
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-12 12:58:18 +0900

    docs: 배열의 요솟값 초기화하여 배열 선언

---
+++ src/doitAlgorithm/chapert01/ex/l2_1/IntArrayInit.java
@@ -0,0 +1,11 @@
+package doitAlgorithm.chapert01.ex.l2_1;
+
+public class IntArrayInit {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i+ "] = " + a[i]);
+        }
+    }
+}
