84fbdc217995e23d6e4fc34ebca3032e9ccbbf07
PlutoWooSeok <plutoxxx1014@gmail.com>
2024-01-18 10:46:39 +0900

    Baekjoon_2908 상수

--- src/codingTest/Baekjoon_2908.java
+++ src/codingTest/Baekjoon_2908.java
@@ -1,29 +1,33 @@
 package codingTest;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Baekjoon_1152 {
+public class Baekjoon_2908 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
+
+        int A = in.nextInt();
+        int B = in.nextInt();
+
         in.close();
 
-        int answer = 0;
-        input = input.trim();
-        if (input.equals("")){
-            System.out.println(answer);
-        }
-        else {
-            String[] result = input.split(" ");
-            System.out.println(result.length);
-        }
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+
+        System.out.print(A > B ? A : B);
     }
 }
 
 
-/*해당 문제는 3차시도 만에 맞췄다. 틀렸던 이유는 split() 매소드를 사용할 때 문자열의 앞뒤 공백이 있을 수 있다는 예외사항을 고려하지 못했으며,
-* 이후 앞뒤 공백은 제거하였으나, ex) String str = ""; 의 경우 문자열의 길이는 1로 표현된다는 점을 고려하지 못하여 2차 시도 또한 실패하였다.
-* 최종 3차 시도로는 모든 예외 사항을 정리 후 진행 => 결과적으론 성공했다.
+/*
+이 문제에서는 StringBuilder를 사용했다.
+
+먼저 StringBuilder 는 문자열을 다루는 클래스로 많이 쓰이고 있는데, 여기서 reverse() 라는 아주 좋은 메소드를 포함하고 있다.
+즉, 위를 사용하기 위해 먼저 StringBuilder 생성과 동시에 append() 라는 메소드에 값을 넣어준다.
+
+이때 append() 로 넣어진 값은 타입이 StringBuilder 라는 타입으로 변환된다.
+그리고 그렇게 저장된 수를 reverse() 라는 메소드를 통해 저장되어있던 문자열을 뒤집는다. 그리고 StringBuilder 타입을 문자열로 반환시키기 위해 toString() 을 써주면 끝이다.
+그리고 문자열로 반환시켰으니 Integer.parseInt() 로 String 을 int 로 타입을 변경시키면 끝이다.
+그렇게 뒤집힌 값은 출력에서 A 와 B 중 큰 값이 출력되도록 하면 끝난다.
 */
