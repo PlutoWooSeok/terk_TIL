768f99f71cd4ac58d3222b6db3b884c101f66a03
wooseok <plutoxxx1014@gmail.com>
2024-10-23 16:25:31 +0900

    2024-10-23_ASP(BOJ)

--- src/codingTest/BOJ1715_wooseok.java
+++ src/codingTest/BOJ1715_wooseok.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-public class BOJ1715 {
+public class BOJ1715_wooseok {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
